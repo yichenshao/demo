737ead14548a103e0a58bf9e05253f01329d6650
yichenshao <yichenshao@umail.ucsb.edu>
2018-05-03 16:34:08 -0700

    update

---
+++ Assets/scripts/BulletDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+	void OnTriggerEnter2D(Collider2D co){
+		if (co.tag == "Zombie") {
+			co.GetComponent<health> ().doDamage (1);
+			Destroy (gameObject);
+		}
+	}
+
+}
---
+++ Assets/scripts/Firing.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firing : MonoBehaviour {
+	public GameObject bulletPrefab;
+	public float interval = 0.5f;
+	void Start(){
+		InvokeRepeating ("Shoot", 0, interval);
+	}
+	bool zombieInFront(){
+		Vector2 origin = new Vector2 (9.5f, transform.position.y);
+		RaycastHit2D[] hits = Physics2D.RaycastAll (origin, -Vector2.right);
+		foreach (RaycastHit2D hit in hits) {
+			if (hit.collider != null && hit.collider.gameObject.tag == "Zombie")
+				return true;
+		}
+		return false;
+	}
+	void Shoot(){
+		if(zombieInFront()){
+			GetComponent<Animator> ().SetTrigger ("IsFiring");
+			Instantiate (bulletPrefab, transform.position, Quaternion.identity);
+		}
+	}
+
+
+}
